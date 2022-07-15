35aa352da9ecc7c79ef52c3a4d932dfc9c4da99c
kaneki2610 <kaneki146261@gmail.com>
2022-07-15 08:44:46 +0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import './style.css';
-
 import { of, map, Observable } from 'rxjs';
-
-of('World')
-  .pipe(map((name) => `Hellodsd, ${name}!`))
-  .subscribe(console.log);
-
-// Open the console in the bottom right to see results.
+function timeout(millisecond) {
+  return new Promise((successFn) => {
+    setTimeout(() => {
+      successFn('Data');
+    }, millisecond);
+  });
+}
+timeout(1000).then((data) => {
+  console.log('data', data);
+}); // Open the console in the bottom right to see results.
